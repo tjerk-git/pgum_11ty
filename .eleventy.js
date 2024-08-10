d2b042073f9ebe1e1cabbdbb74791ee4ec6ee5a5
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-08-10 17:01:38 +0200

    push it baby

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+
+const moment = require('moment');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addFilter('timeAgo', function (date) {
+    if (!date) return '';
+    return moment(date).fromNow();
+  });
 };
