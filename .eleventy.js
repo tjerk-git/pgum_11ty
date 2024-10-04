a761dd1b2e9359aed6ee7e9fde2d67787fcb9286
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-10-04 19:01:47 +0200

    added gummetje

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const moment = require('moment');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
+  eleventyConfig.addPassthroughCopy("app.js");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addFilter('timeAgo', function (date) {
     if (!date) return '';
