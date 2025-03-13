48f43e1ba8e274b47c0875a1b241508bb018ec11
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2025-03-13 16:55:15 +0100

    ok

--- .eleventy.js
+++ .eleventy.js
@@ -1,46 +1,15 @@
-const CleanCSS = require("clean-css");
-const Terser = require("terser");
+module.exports = function(eleventyConfig) {
+    // Passthrough copy for static assets
+    eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("app.js");
+    eleventyConfig.addPassthroughCopy("js");
 
-module.exports = function (eleventyConfig) {
-  // Minify CSS
-  eleventyConfig.addFilter("cssmin", function(code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
-
-  // Minify JS
-  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (code, callback) {
-    try {
-      const minified = await Terser.minify(code);
-      callback(null, minified.code);
-    } catch (err) {
-      console.error("Terser error: ", err);
-      callback(null, code);
-    }
-  });
-
-  // Pass through files
-  eleventyConfig.addPassthroughCopy({"images": "images"});
-  
-  // Process and minify CSS and JS files
-  eleventyConfig.addTemplateFormats("css");
-  eleventyConfig.addTemplateFormats("js");
-  
-  eleventyConfig.addExtension("css", {
-    outputFileExtension: "css",
-    compile: async function(inputContent) {
-      return async () => {
-        return new CleanCSS({}).minify(inputContent).styles;
-      };
-    }
-  });
-  
-  eleventyConfig.addExtension("js", {
-    outputFileExtension: "js",
-    compile: async function(inputContent) {
-      return async () => {
-        const minified = await Terser.minify(inputContent);
-        return minified.code;
-      };
-    }
-  });
+    return {
+        dir: {
+            input: ".",
+            output: "_site",
+            includes: "_includes"
+        }
+    };
 };
